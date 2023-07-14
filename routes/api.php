5bef35616363f8aa086b5803e2e801fc3c28ccbc
Buzulan Dumitru <bdumitrua@gmail.com>
2023-07-14 19:50:59 +0300

    connected controllers to prefixes

--- routes/api.php
+++ routes/api.php
@@ -52,133 +52,120 @@ Route::prefix('users')->group(function () {
     });
 });
 
-Route::prefix('address')->group(function () {
-    Route::controller(ApiAddressController::class)->group(function () {
-        Route::middleware(['auth:api'])->group(function () {
-            // Создание адресса в списке адрессов пользоваля
-            Route::post('create', 'create');
-            // Изменение адресса
-            Route::post('update/{id}', 'update');
-            // Удаление адресса
-            Route::post('delete/{id}', 'delete');
-        });
+Route::prefix('address')->controller(ApiAddressController::class)->group(function () {
+    Route::middleware(['auth:api'])->group(function () {
+        // Создание адресса в списке адрессов пользоваля
+        Route::post('create', 'create');
+        // Изменение адресса
+        Route::post('update/{id}', 'update');
+        // Удаление адресса
+        Route::post('delete/{id}', 'delete');
     });
 });
 
-Route::prefix('cart')->group(function () {
-    Route::controller(ApiCartController::class)->group(function () {
-        Route::middleware(['auth:api'])->group(function () {
-            // Получить все товары в корзине
-            Route::get('/', 'index');
-            // Добавить товар в корзину 
-            Route::post('create/{id}', 'store');
-            // Увеличить кол-во товара на 1
-            Route::patch('increase/{id}', 'increase');
-            // Уменьшить кол-во товара на 1
-            Route::patch('decrease/{id}', 'decrease');
-            // Установить кол-во на quantity
-            Route::patch('update', 'update');
-            // Удалить из корзины
-            Route::delete('delete/{id}', 'destroy');
-        });
-    });
-});
-
-Route::prefix('favorites')->group(function () {
-    Route::controller(ApiUserFavoriteController::class)->group(function () {
-        Route::middleware(['auth:api'])->group(function () {
-            // Получить все товары в корзине
-            Route::get('/', 'index');
-            // Добавить товар в корзину 
-            Route::post('store/{id}', 'store');
-            // Удалить из корзины
-            Route::delete('remove/{id}', 'destroy');
-        });
-    });
-});
-
-Route::prefix('category')->group(function () {
-    Route::controller(ApiCategoryController::class)->group(function () {
-        // Получение всех категорий
+Route::prefix('cart')->controller(ApiCartController::class)->group(function () {
+    Route::middleware(['auth:api'])->group(function () {
+        // Получить все товары в корзине
         Route::get('/', 'index');
-        // Получение всех товаров id категории
-        Route::get('/byid/{id}', 'getProductsByCategoryId');
-        // Получение всех товаров по slug-у категории
-        Route::get('{slug}', 'getProductsBySlug');
-
-        Route::middleware(['auth:api', 'role:4'])->group(function () {
-            // Создание категории
-            Route::post('create', 'store');
-            // Изменение категории
-            Route::put('update/{id}', 'update');
-            // Удаление категории
-            Route::delete('delete/{id}', 'destroy');
-        });
+        // Добавить товар в корзину 
+        Route::post('create/{id}', 'store');
+        // Увеличить кол-во товара на 1
+        Route::patch('increase/{id}', 'increase');
+        // Уменьшить кол-во товара на 1
+        Route::patch('decrease/{id}', 'decrease');
+        // Установить кол-во на quantity
+        Route::patch('update', 'update');
+        // Удалить из корзины
+        Route::delete('delete/{id}', 'destroy');
     });
 });
-Route::prefix('orders')->group(function () {
-    Route::controller(ApiOrderController::class)->group(function () {
-        Route::middleware(['auth:api'])->group(function () {
-            // получить все свои заказы
-            Route::get('/', 'index');
 
-            // получить конкретный заказ
-            Route::get('/{id}', 'show');
-
-            // Создать заказ
-            Route::post('create', 'store');
-        });
+Route::prefix('favorites')->controller(ApiUserFavoriteController::class)->group(function () {
+    Route::middleware(['auth:api'])->group(function () {
+        // Получить все товары в корзине
+        Route::get('/', 'index');
+        // Добавить товар в корзину 
+        Route::post('store/{id}', 'store');
+        // Удалить из корзины
+        Route::delete('remove/{id}', 'destroy');
     });
 });
-Route::prefix('products')->group(function () {
-    Route::controller(ApiProductController::class)->group(function () {
-        // Получить продукт (по id)
+
+Route::prefix('category')->controller(ApiCategoryController::class)->group(function () {
+    // Получение всех категорий
+    Route::get('/', 'index');
+    // Получение всех товаров id категории
+    Route::get('/byid/{id}', 'getProductsByCategoryId');
+    // Получение всех товаров по slug-у категории
+    Route::get('{slug}', 'getProductsBySlug');
+
+    Route::middleware(['auth:api', 'role:4'])->group(function () {
+        // Создание категории
+        Route::post('create', 'store');
+        // Изменение категории
+        Route::put('update/{id}', 'update');
+        // Удаление категории
+        Route::delete('delete/{id}', 'destroy');
+    });
+});
+
+Route::prefix('orders')->controller(ApiOrderController::class)->group(function () {
+    Route::middleware(['auth:api'])->group(function () {
+        // получить все свои заказы
+        Route::get('/', 'index');
+
+        // получить конкретный заказ
         Route::get('/{id}', 'show');
-        // Получить все продукты
-        Route::get('/getall', 'index');
-        // Получить похожие продукты (продукты той-же категории с небольшим отличием по цене (+-30%))
-        Route::get('/similar/{id}', 'similar');
 
-        Route::middleware(['auth:api', 'role:3'])->group(function () {
-            // Создать продукт
-            Route::post('create', 'store');
-            // Изменить продукт по id 
-            Route::patch('update/{id}', 'update');
-            // Удалить продукт 
-            Route::delete('delete/{id}', 'destroy');
-        });
+        // Создать заказ
+        Route::post('create', 'store');
     });
 });
-Route::prefix('reviews')->group(function () {
-    Route::controller(ApiReviewController::class)->group(function () {
-        // Получить вообще все отзывы
-        Route::get('/', 'index');
-        // Получить все отзывы товара (по id)
-        Route::get('/product/{id}', 'getProductReviews');
-        // Получить среднюю оценку товара (по id)
-        Route::get('/productavg/{id}', 'getAverageRating');
-        // Получить отзывы товаров (по категории)
-        Route::get('/category/{id}', 'getCategoryReviews');
 
-        Route::middleware(['auth:api'])->group(function () {
-            // Оставить отзыв
-            Route::post('create', 'store');
-            // Отредактировать отзыв
-            Route::patch('update/{id}', 'update');
-            // Удалить отзыв
-            Route::delete('delete/{id}', 'destroy');
-        });
+Route::prefix('products')->controller(ApiProductController::class)->group(function () {
+    // Получить продукт (по id)
+    Route::get('/show/{id}', 'show');
+    // Получить все продукты
+    Route::get('/getall', 'index');
+    // Получить похожие продукты (продукты той-же категории с небольшим отличием по цене (+-30%))
+    Route::get('/similar/{id}', 'similar');
+
+    Route::middleware(['auth:api', 'role:3'])->group(function () {
+        // Создать продукт
+        Route::post('create', 'store');
+        // Изменить продукт по id 
+        Route::patch('update/{id}', 'update');
+        // Удалить продукт 
+        Route::delete('delete/{id}', 'destroy');
     });
+});
 
-    Route::prefix('votes')->group(function () {
-        Route::controller(ApiReviewVoteController::class)->group(function () {
-            Route::middleware(['auth:api'])->group(function () {
-                Route::get('/', 'index');
-                Route::post('like/{id}', 'like');
-                Route::post('dislike/{id}', 'dislike');
-                Route::delete('remove/{id}', 'destroy');
-            });
-        });
+Route::prefix('reviews')->controller(ApiReviewController::class)->group(function () {
+    // Получить вообще все отзывы
+    Route::get('/', 'index');
+    // Получить все отзывы товара (по id)
+    Route::get('/product/{id}', 'getProductReviews');
+    // Получить среднюю оценку товара (по id)
+    Route::get('/productavg/{id}', 'getAverageRating');
+    // Получить отзывы товаров (по категории)
+    Route::get('/category/{id}', 'getCategoryReviews');
+
+    Route::middleware(['auth:api'])->group(function () {
+        // Оставить отзыв
+        Route::post('create', 'store');
+        // Отредактировать отзыв
+        Route::patch('update/{id}', 'update');
+        // Удалить отзыв
+        Route::delete('delete/{id}', 'destroy');
+    });
+});
+
+Route::prefix('reviews/votes')->controller(ApiReviewVoteController::class)->group(function () {
+    Route::middleware(['auth:api'])->group(function () {
+        Route::get('/', 'index');
+        Route::post('like/{id}', 'like');
+        Route::post('dislike/{id}', 'dislike');
+        Route::delete('remove/{id}', 'destroy');
     });
 });
 
@@ -189,29 +176,27 @@ Route::prefix('reviews')->group(function () {
 // 4 - ADMIN
 // 5 - HEAD_ADMIN
 
-Route::prefix('roles')->group(function () {
-    Route::controller(ApiUserRoleController::class)->group(function () {
-        // Маршруты, доступные только для мэнэджера 
-        // Просмотр ролей и создание/удаление права на торговлю свои товаром 
-        Route::middleware(['auth:api', 'role:3'])->group(function () {
-            Route::get('get/{user_id}', 'index');
-            Route::post('make/seller/{user_id}', 'makeSaller');
-            Route::post('remove/seller/{user_id}', 'removeSeller');
-        });
+Route::prefix('roles')->controller(ApiUserRoleController::class)->group(function () {
+    // Маршруты, доступные только для мэнэджера 
+    // Просмотр ролей и создание/удаление права на торговлю свои товаром 
+    Route::middleware(['auth:api', 'role:3'])->group(function () {
+        Route::get('get/{user_id}', 'index');
+        Route::post('make/seller/{user_id}', 'makeSaller');
+        Route::post('remove/seller/{user_id}', 'removeSeller');
+    });
 
-        // Дополнительные команды, доступные только админам
-        Route::middleware(['auth:api', 'role:4'])->group(function () {
-            Route::post('make/manager/{user_id}', 'makeManager');
-            Route::post('remove/manager/{user_id}', 'removeManager');
+    // Дополнительные команды, доступные только админам
+    Route::middleware(['auth:api', 'role:4'])->group(function () {
+        Route::post('make/manager/{user_id}', 'makeManager');
+        Route::post('remove/manager/{user_id}', 'removeManager');
 
-            // Полная очистка всех ролей, делает пользователя обычным USER
-            // Работает только на пользователей с ROLE < совершающего очистку
-            Route::put('clear/{user_id}', 'destroy');
-        });
+        // Полная очистка всех ролей, делает пользователя обычным USER
+        // Работает только на пользователей с ROLE < совершающего очистку
+        Route::put('clear/{user_id}', 'destroy');
+    });
 
-        Route::middleware(['auth:api', 'role:5'])->group(function () {
-            Route::post('make/admin/{user_id}', 'makeAdmin');
-            Route::post('remove/admin/{user_id}', 'removeAdmin');
-        });
+    Route::middleware(['auth:api', 'role:5'])->group(function () {
+        Route::post('make/admin/{user_id}', 'makeAdmin');
+        Route::post('remove/admin/{user_id}', 'removeAdmin');
     });
 });
