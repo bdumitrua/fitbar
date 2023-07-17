e7c9299859ca90be3e2b98945a20e568ec8ac1cd
Buzulan Dumitru <bdumitrua@gmail.com>
2023-07-17 19:56:46 +0300

    Refactored address controllers

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AddressRequest;
+use App\Models\Address;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AddressController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = User::find(Auth::id());
+        return $user->addresses;
+    }
+
+    // Создание нового адреса
+    public function create(AddressRequest $request)
+    {
+        $address = Address::create([
+            'address' => $request->address,
+            'user_id' => Auth::id(),
+        ]);
+
+        return $address;
+    }
+
+    // Обновление существующего адреса
+    public function update(AddressRequest $request, Address $address)
+    {
+        $user = User::findOrFail(Auth::id());
+        $addressUpdateStatus = $user->addresses()->where('id', $address->id)->update([
+            'address' => $request->address
+        ]);
+
+        return $addressUpdateStatus;
+    }
+
+    // Удаление адреса
+    public function delete(Address $address)
+    {
+        $user = User::find(Auth::id());
+        $addressDeleteStatus = $user->addresses()->where('id', $address->id)->delete();
+
+        return $addressDeleteStatus;
+    }
 }
--- app/Http/Controllers/Api/ApiAddressController.php
+++ app/Http/Controllers/Api/ApiAddressController.php
@@ -2,28 +2,25 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\AddressController;
 use App\Http\Requests\AddressRequest;
 use App\Models\Address;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class ApiAddressController extends Controller
+class ApiAddressController extends AddressController
 {
     public function index()
     {
-        $user = User::find(Auth::id());
-        return response()->json($user->addresses);
+        $addresses = parent::index();
+        return response()->json($addresses);
     }
 
     // Создание нового адреса
     public function create(AddressRequest $request)
     {
-        $address = Address::create([
-            'address' => $request->address,
-            'user_id' => Auth::id(),
-        ]);
+        $address = parent::create($request);
 
         return response()->json([
             'status' => 'success',
@@ -34,12 +31,9 @@ class ApiAddressController extends Controller
     // Обновление существующего адреса
     public function update(AddressRequest $request, Address $address)
     {
-        $user = User::find(Auth::id());
-        $address = $user->addresses()->where('id', $address->id)->update([
-            'address' => $request->address
-        ]);
+        $addressUpdateStatus = parent::update($request, $address);
 
-        if (!$address) {
+        if (!$addressUpdateStatus) {
             return response()->json([
                 'message' => 'access denied'
             ], 404);
@@ -54,10 +48,9 @@ class ApiAddressController extends Controller
     // Удаление адреса
     public function delete(Address $address)
     {
-        $user = User::find(Auth::id());
-        $address = $user->addresses()->where('id', $address->id)->delete();
+        $addressDeleteStatus = parent::delete($address);
 
-        if (!$address) {
+        if (!$addressDeleteStatus) {
             return response()->json([
                 'message' => 'access denied'
             ], 404);
