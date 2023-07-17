fe34ef44a976d59e04a985d164b106b41a43667e
Buzulan Dumitru <bdumitrua@gmail.com>
2023-07-17 21:51:06 +0300

    Added convert main response to JSON response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,13 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected function MainResponseToJSON($response)
+    {
+        if (isset($response['error'])) {
+            return response()->json(['error' => $response['error']], $response['code']);
+        }
+
+        return response()->json(['message' => $response['message']], $response['code']);
+    }
 }
