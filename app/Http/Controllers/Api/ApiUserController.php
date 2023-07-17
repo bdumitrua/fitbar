d48722278315ea1ff831f165362d47200703a04b
Buzulan Dumitru <bdumitrua@gmail.com>
2023-07-17 22:41:05 +0300

    refactored user controllers

--- app/Http/Controllers/Api/ApiUserController.php
+++ app/Http/Controllers/Api/ApiUserController.php
@@ -3,45 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\UserController;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 
-class ApiUserController extends Controller
+class ApiUserController extends UserController
 {
     public function me()
     {
-        return User::find(Auth::id())->load('roles', 'orders', 'reviews', 'addresses', 'cart');
+        $response = parent::me();
+
+        return parent::MainResponseToJSON($response);
     }
 
     public function update(UserRequest $request)
     {
-        $user = User::find(Auth::id());
-        if (User::where('email', $request->email)->count() > 0 && $user->email != $request->email) {
-            throw ValidationException::withMessages([
-                'Данная почта уже занята другим пользователем.'
-            ], 421);
-        }
+        $response = parent::update($request);
 
-        $path = '';
-        // загрузка фото, если оно предоставлено
-        if ($request->hasFile('photo')) {
-            $path = $request->file('photo')->store('profiles', 'public');
-        }
-
-        $user->update([
-            'name' => $request->name,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'photo' => $path,
-        ]);
-
-        // возвращаем обновленного пользователя
-        return response()->json([
-            'status' => 'success',
-            'user' => $user
-        ]);
+        return parent::MainResponseToJSON($response);
     }
 }
