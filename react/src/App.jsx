11a6adbed229c57f1b888cbc0ca7d769b1221229
Skobelev Matvey <mask0343@gmail.com>
2023-07-19 17:59:57 +0400

    Created files for mixins and variables for more comfortable using. Header html ready

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,13 @@
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
 import './styles/App.scss'
 
 function App() {
 
   return (
     <div>
-      App
+      <Header />
+      <Home />
     </div>
   )
 }
---
+++ react/src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Header.scss'
+
+const Header = () => {
+  return (
+    <header className="header">
+        <div className="header__container">
+            <div className="header__left-side">
+                <img src="../../images/logo.png" alt="" className="header__logo" />
+                <span className="header__site-name">fitbar</span>
+            </div>
+            <input type="search" className="header__search" />
+            <div className="header__right-side">
+                <a href="" className="header__account-button">
+                    <img src="../../images/account.png" alt="" className="header__account-button-image" />
+                    <a href="" className="header__account-button-text">Аккаунт</a>
+                </a>
+                <a href="" className="header__cart-button">
+                    <img src="../../images/cart.svg" alt="" className="header__cart-button-image" />
+                    <a href="" className="header__cart-button-text">Корзина</a>
+                </a>    
+            </div>    
+        </div>
+        <nav className="header__navbar">
+            <a href="" className="header__navbar-element">Soon fetching elements</a>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
