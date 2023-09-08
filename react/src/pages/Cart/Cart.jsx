77d2c797803483df6c46a057c21e353c2ee12760
Skobelev Matvey <mask0343@gmail.com>
2023-09-08 19:24:29 +0400

    Fix cart total price

--- react/src/pages/Cart/Cart.jsx
+++ react/src/pages/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import "./Cart.scss";
 import CartCard from "./CartCard/CartCard";
 
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../utils/providers/cart.provider";
 import CartTotalPrice from "./CartTotalPrice";
@@ -9,15 +10,28 @@ import CartTotalPrice from "./CartTotalPrice";
 // Сделать обработку корзины с редаксом
 
 const Cart = () => {
-    const { handleRemoveFromCart, cartItems } = useCartContext();
+    const { handleRemoveFromCart, cartItems, setCartItems } = useCartContext();
 
-    const totalPrice = cartItems.reduce((accumulator, currentItem) => {
-        return (
-            +accumulator +
-            +currentItem.price *
-                localStorage.getItem(`product_count_${currentItem.id}`)
-        );
-    }, 0);
+    // Состояние для хранения общей суммы
+    const [totalPrice, setTotalPrice] = useState(0);
+
+    const loadCartData = () => {
+        const items = JSON.parse(localStorage.getItem("cart")) || [];
+        setCartItems(items);
+
+        const newTotalPrice = items.reduce((accumulator, currentItem) => {
+            return (
+                +accumulator +
+                +currentItem.price *
+                    localStorage.getItem(`product_count_${currentItem.id}`)
+            );
+        }, 0);
+        setTotalPrice(newTotalPrice);
+    };
+
+    useEffect(() => {
+        loadCartData(); // Загрузите данные из локального хранилища
+    }, []);
 
     return (
         <div className="cart container">
@@ -27,6 +41,7 @@ const Cart = () => {
                     product={product}
                     key={product.id}
                     handleRemoveFromCart={handleRemoveFromCart}
+                    updateTotalPrice={loadCartData}
                 />
             ))}
             {cartItems.length == 0 && (
--- react/src/pages/Cart/CartCard/CartCard.jsx
+++ react/src/pages/Cart/CartCard/CartCard.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 import deleteFromCart from "../../../assets/images/delete.svg";
 import axiosInstance from "../../../utils/axios/instance";
 
-const CartCard = ({ product, handleRemoveFromCart }) => {
+const CartCard = ({ product, handleRemoveFromCart, updateTotalPrice }) => {
     const [productCount, setProductCount] = useState(
         +localStorage.getItem(`product_count_${product.id}` || 1)
     );
@@ -16,16 +16,21 @@ const CartCard = ({ product, handleRemoveFromCart }) => {
             const newCount = prevCount + 1;
             axiosInstance.patch(`/cart/increase/${product.id}`);
             localStorage.setItem(`product_count_${product.id}`, newCount);
+            updateTotalPrice(); // Добавьте это
             return newCount;
         });
     };
 
     const decrementProductCount = () => {
         setProductCount((prevCount) => {
-            const newCount = prevCount - 1;
-            axiosInstance.patch(`/cart/decrease/${product.id}`);
-            localStorage.setItem(`product_count_${product.id}`, newCount);
-            return newCount;
+            if (prevCount > 1) {
+                const newCount = prevCount - 1;
+                axiosInstance.patch(`/cart/decrease/${product.id}`);
+                localStorage.setItem(`product_count_${product.id}`, newCount);
+                updateTotalPrice(); // Добавьте это
+                return newCount;
+            }
+            return prevCount;
         });
     };
 
